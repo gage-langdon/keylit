1783afab3fe18f8f5c6e4ca7c59975433eba14a3
Gage <gagelangdon@gmail.com>
2021-09-21 02:53:15 -0700

    add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,34 +10,47 @@ const IndexPage = () => {
   const { hex: backgroundColor, secondaryColor } = selectedColor
 
   return (
-    <Layout>
-      <SEO
-        title="A key light for Zoom, FaceTime, and TikTok videos"
-        description="A free keylight to light your face in a Zoom, FaceTime, or TikTok video"
-      />
-      <div
-        style={{
-          backgroundColor,
-          height: "calc(100vh)",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
+    <>
+      <Layout>
+        <SEO
+          title="A key light for Zoom, FaceTime, and TikTok videos"
+          description="A free keylight to light your face in a Zoom, FaceTime, or TikTok video"
+        />
         <div
           style={{
-            marginBottom: "auto",
-            paddingTop: "16px",
+            backgroundColor,
+            height: "calc(100vh)",
+            width: "100%",
             display: "flex",
             flexDirection: "column",
-            marginLeft: "auto",
-            width: "100%",
           }}
         >
-          <ColorPicker onChange={setbgColor} value={selectedColor} />
+          <div
+            style={{
+              marginBottom: "auto",
+              paddingTop: "16px",
+              display: "flex",
+              flexDirection: "column",
+              marginLeft: "auto",
+              width: "100%",
+            }}
+          >
+            <ColorPicker onChange={setbgColor} value={selectedColor} />
+          </div>
         </div>
+      </Layout>
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          width: "100vw",
+          color: secondaryColor || "white",
+        }}
+      >
+        Built by Ian and Gage
       </div>
-    </Layout>
+    </>
   )
 }
 
